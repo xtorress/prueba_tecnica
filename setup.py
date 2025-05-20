e8fcbee480630c6cf10fc1f478f0e74699cc645d
xiomi <xiomara.torresg01@gmail.com>
2025-05-20 18:13:46 +0200

    Fix structure project.

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="prueba_tecnica",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(where="app"),
+    package_dir={"": "app"},
 )
