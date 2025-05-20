73e4d8eaad85e19a7b9294dc7314323150eea3f1
xiomi <xiomara.torresg01@gmail.com>
2025-05-20 20:09:41 +0200

    Implement logging.

---
+++ app/logging_config.py
@@ -0,0 +1,17 @@
+import logging
+
+def setup_logger(name="Market_Simulation"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s | %(message)s')
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        file_handler = logging.FileHandler("simulation.log", encoding="utf-8")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
