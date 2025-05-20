22784a2707157b5695c31208e040a8ef0f6af2a6
xiomi <xiomara.torresg01@gmail.com>
2025-05-20 21:18:52 +0200

    Modify buy_card() and sell_card()

--- app/models/market.py
+++ app/models/market.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod, ABC
+from decimal import Decimal
 import random
 
 from logging_config import setup_logger
@@ -6,24 +7,34 @@ from logging_config import setup_logger
 logger = setup_logger(__name__)
 
 class Market:
-    def __init__(self, start_price=200, stock=15):
-        self.start_price = start_price
-        self.stock = stock
-        self.current_price = start_price
-        self.previous_price = start_price
+    PRICE_INCREASE = Decimal('1.005')
+    PRICE_DECREASE = Decimal('0.995')
+
+    def __init__(self, start_price=200, stock=100000):
+        # self.start_price = start_price
+        self.stock: int = stock
+        self.current_price: Decimal = Decimal(str(start_price))
+        self.previous_price: Decimal = Decimal(str(start_price))
 
     def buy_card(self):
-        self.stock += 1
-        self.current_price -= 0.005 * self.current_price
+        """
+        Process the purchase of a graphics card
+        """
+        if self.stock <= 0:
+            logger.warning("No hay stock.")
+            return -1
+
+        self.stock -= 1
+        self.current_price *= self.PRICE_INCREASE
+        return 1
 
     def sell_card(self):
-        if self.stock > 0:
-            self.stock -= 1
-            self.current_price += 0.005 * self.current_price
-            return True
-        else:
-            logger.warning("No hay stock.")
-            return False
+        """
+        Process the sale of a graphics card.
+        """
+        self.stock += 1
+        self.current_price *= self.PRICE_DECREASE
+        return 1
 
     def get_change_price(self) -> float:
         return ((self.current_price - self.start_price) / self.start_price)
--- app/tests/test_market.py
+++ app/tests/test_market.py
@@ -1,2 +1,26 @@
 import pytest
 
+from models.market import Market
+
+@pytest.fixture
+def TheMarket():
+    return Market(200, 15)
+
+def test_buy_card(TheMarket):
+    TheMarket.buy_card()
+    assert TheMarket.stock == 14
+    assert TheMarket.current_price == 201
+
+def test_sell_card(TheMarket):
+    TheMarket.sell_card()
+    assert TheMarket.stock == 16
+    assert TheMarket.current_price == 199
+
+def test_sell_card_no_stock(TheMarket):
+    pass
+
+def test_get_change_price(TheMarket):
+    pass
+
+def test_update_market(TheMarket):
+    pass
