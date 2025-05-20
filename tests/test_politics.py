dd35f1029eba9537b6cbb1a0f1e761c393c5b5d7
xiomi <xiomara.torresg01@gmail.com>
2025-05-20 11:48:09 +0200

    Update tests for politics.

--- tests/test_politics.py
+++ tests/test_politics.py
@@ -1,36 +1,47 @@
 import pytest
 import random
+from unittest.mock import patch, MagicMock
 
 from models.politics import RandomPolitic, TrendPolitic, AntiTrendPolitic, PersonalPolitic
+from models.market import Market
 
-def test_random_politic_returns_valid_action():
-    policy = RandomPolitic()
-    action = policy.set_politc(change_price=0.05)
-    assert action in ["BUY", "SELL", "PASS"], f"{action}"
+@pytest.fixture
+def Marketplace():
+    return Market(stock=10)
+
+# def test_random_politic_returns_valid_action():
+#     policy = RandomPolitic()
+
+#     action = policy.action(change_price=0.05)
+
+#     assert action in ["BUY", "SELL", "PASS"], f"{action}"
 
 def test_trend_politic_buy_when_positive_trend():
+    market = MagicMock()
+    market.get_change_price.return_value = 0.01
     policy = TrendPolitic()
-    action = policy.set_politc(change_price=0.01)
-    # Con la semilla fija, debe dar 'BUY' o 'PASS'
+    action = policy.action(market)
     assert action in ["BUY", "PASS"]
 
 def test_trend_politic_sell_when_not_trending():
+    market = MagicMock()
+    market.get_change_price.return_value = 0.005
     policy = TrendPolitic()
-    action = policy.set_politc(change_price=0.005)
+    action = policy.action(market)
     assert action in ["PASS", "SELL"]
 
 def test_antitrend_politic_buy_when_negative_trend():
+    market = MagicMock()
+    market.get_change_price.return_value = -0.01
     policy = AntiTrendPolitic()
-    action = policy.set_politc(change_price=-0.01)
+    action = policy.action(market)
     assert action in ["BUY", "PASS"]
 
 def test_antitrend_politic_sell_when_positive_trend():
+    market = MagicMock()
+    market.get_change_price.return_value = 0.02
     policy = AntiTrendPolitic()
-    action = policy.set_politc(change_price=0.02)
+    action = policy.action(market)
     assert action in ["PASS", "SELL"]
 
-def test_personal_politic_implemented():
-    # Como está vacío, solo verificamos que no lance errores y devuelve None
-    policy = PersonalPolitic()
-    result = policy.set_politc(change_price=0.05)
-    assert result is None
+# test personal agent
