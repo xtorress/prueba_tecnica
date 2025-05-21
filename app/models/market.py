970d07d689aa7345831796b160a537a201d49435
xiomi <xiomara.torresg01@gmail.com>
2025-05-21 05:03:20 +0200

    Modify Market class.

--- app/models/market.py
+++ app/models/market.py
@@ -7,37 +7,58 @@ from logging_config import setup_logger
 logger = setup_logger(__name__)
 
 class Market:
+    """Market class. Represents the graphics card market"""
     PRICE_INCREASE = Decimal('1.005')
     PRICE_DECREASE = Decimal('0.995')
 
     def __init__(self, start_price=200, stock=100000):
-        # self.start_price = start_price
-        self.stock: int = stock
-        self.current_price: Decimal = Decimal(str(start_price))
-        self.previous_price: Decimal = Decimal(str(start_price))
+        self._stock: int = stock
+        self._current_price: Decimal = Decimal(str(start_price))
+        self._previous_price: Decimal = Decimal(str(start_price))
 
-    def buy_card(self):
+    @property
+    def stock(self) -> int:
+        return self._stock
+    
+    @property
+    def current_price(self) -> Decimal:
+        return self._current_price
+    
+    @property
+    def previous_price(self) -> Decimal:
+        return self._previous_price
+    
+    def _adjust_price(self, percent: Decimal) -> None:
+        self._current_price *= percent
+
+    def buy_card(self) -> bool:
         """
         Process the purchase of a graphics card
         """
-        if self.stock <= 0:
+        if self._stock <= 0:
             logger.warning("No hay stock.")
-            return -1
+            return False
 
-        self.stock -= 1
-        self.current_price *= self.PRICE_INCREASE
-        return 1
+        self._stock -= 1
+        self._adjust_price(self.PRICE_INCREASE)
+        return True
 
-    def sell_card(self):
+    def sell_card(self) -> bool:
         """
         Process the sale of a graphics card.
         """
-        self.stock += 1
-        self.current_price *= self.PRICE_DECREASE
-        return 1
+        self._stock += 1
+        self._adjust_price(self.PRICE_DECREASE)
+        return True
 
-    def get_change_price(self) -> float:
-        return ((self.current_price - self.start_price) / self.start_price)
+    def get_change_price(self) -> Decimal:
+        """
+        Calculates the change in price with respect to the previous iteration.
+        """
+        if self.previous_price == 0:
+            logger.warning('Precio es 0.')
+            return Decimal('0.000')
+        return round((self._current_price - self._previous_price) / self._previous_price, 3)
     
     def update_market(self):
-        self.previous_price = self.current_price
+        self._previous_price = self._current_price
--- app/tests/test_market.py
+++ app/tests/test_market.py
@@ -1,10 +1,16 @@
 import pytest
+from decimal import Decimal
 
 from models.market import Market
 
 @pytest.fixture
 def TheMarket():
     return Market(200, 15)
+
+def test_adjust_price(TheMarket):
+    percent = Decimal('1.005')
+    TheMarket._adjust_price(percent)
+    assert TheMarket.current_price == 201
 
 def test_buy_card(TheMarket):
     TheMarket.buy_card()
@@ -19,8 +25,23 @@ def test_sell_card(TheMarket):
 def test_sell_card_no_stock(TheMarket):
     pass
 
-def test_get_change_price(TheMarket):
-    pass
+def test_get_change_price_down(TheMarket):
+    TheMarket._previous_price = Decimal('201')
+    TheMarket._current_price = Decimal('200')
+    result = TheMarket.get_change_price()
+    assert result == Decimal('-0.005')
+
+def test_get_change_price_up(TheMarket):
+    TheMarket._previous_price = Decimal('199')
+    TheMarket._current_price = Decimal('200')
+    result = TheMarket.get_change_price()
+    assert result == Decimal('0.005')
+
+def test_get_change_price_up(TheMarket):
+    TheMarket._previous_price = Decimal('200')
+    TheMarket._current_price = Decimal('200')
+    result = TheMarket.get_change_price()
+    assert result == Decimal('0.00')
 
 def test_update_market(TheMarket):
     pass
