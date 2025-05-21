5483f439986eded46617f7002c91987080f7f535
xiomi <xiomara.torresg01@gmail.com>
2025-05-21 05:27:31 +0200

    Modify action() to be based on attribute Market._change_price

--- app/models/politics.py
+++ app/models/politics.py
@@ -1,28 +1,27 @@
 from abc import abstractmethod, ABC
+from decimal import Decimal
 import random
-
-from models.market import Market
 
 class Politic(ABC):
     """Abstract class of politics."""
 
     @abstractmethod
-    def action(self, market: Market):
+    def action(self, change_price: Decimal):
         pass
 
 
 class RandomPolitic(Politic):
     """Class for random agent policies."""
 
-    def action(self, market: Market) -> str:
+    def action(self, change_price: Decimal) -> str:
         return random.choices(["BUY", "SELL", "PASS"])[0]
 
 
 class TrendPolitic(Politic):
     """Class for tresnd agent policies."""
     
-    def action(self, market: Market):
-        if market.get_change_price() >= 0.01:
+    def action(self, change_price: Decimal):
+        if change_price >= 0.01:
             return random.choices(["BUY", "PASS"], weights=[0.75, 0.25])[0]
         else:
             return random.choices(["PASS", "SELL"], weights=[0.8, 0.2])[0]
@@ -31,8 +30,8 @@ class TrendPolitic(Politic):
 class AntiTrendPolitic(Politic):
     """Class for anti trend agent policies."""
 
-    def action(self, market: Market):
-        if market.get_change_price() <= -0.01:
+    def action(self, change_price: Decimal):
+        if change_price <= -0.01:
             return random.choices(["BUY", "PASS"], weights=[0.75, 0.25])[0]
         else:
             return random.choices(["PASS", "SELL"], weights=[0.8, 0.2])[0]
@@ -41,5 +40,5 @@ class AntiTrendPolitic(Politic):
 class PersonalPolitic(Politic):
     """Class of policy for agent with logic defined by me."""
 
-    def action(self, market: Market):
+    def action(self, change_price: Decimal):
         pass
--- app/tests/test_politics.py
+++ app/tests/test_politics.py
@@ -1,13 +1,7 @@
 import pytest
-import random
 from unittest.mock import patch, MagicMock
 
 from models.politics import RandomPolitic, TrendPolitic, AntiTrendPolitic, PersonalPolitic
-from models.market import Market
-
-@pytest.fixture
-def Marketplace():
-    return Market(stock=10)
 
 # def test_random_politic_returns_valid_action():
 #     policy = RandomPolitic()
@@ -17,31 +11,27 @@ def Marketplace():
 #     assert action in ["BUY", "SELL", "PASS"], f"{action}"
 
 def test_trend_politic_buy_when_positive_trend():
-    market = MagicMock()
-    market.get_change_price.return_value = 0.01
+    change_price = 0.01
     policy = TrendPolitic()
-    action = policy.action(market)
+    action = policy.action(change_price)
     assert action in ["BUY", "PASS"]
 
 def test_trend_politic_sell_when_not_trending():
-    market = MagicMock()
-    market.get_change_price.return_value = 0.005
+    change_price = 0.005
     policy = TrendPolitic()
-    action = policy.action(market)
+    action = policy.action(change_price)
     assert action in ["PASS", "SELL"]
 
 def test_antitrend_politic_buy_when_negative_trend():
-    market = MagicMock()
-    market.get_change_price.return_value = -0.01
+    change_price = -0.01
     policy = AntiTrendPolitic()
-    action = policy.action(market)
+    action = policy.action(change_price)
     assert action in ["BUY", "PASS"]
 
 def test_antitrend_politic_sell_when_positive_trend():
-    market = MagicMock()
-    market.get_change_price.return_value = 0.02
+    change_price = 0.02
     policy = AntiTrendPolitic()
-    action = policy.action(market)
+    action = policy.action(change_price)
     assert action in ["PASS", "SELL"]
 
 # test personal agent
