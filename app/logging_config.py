071b41600a8d47daaa0966ac58d065346bc41e40
xiomi <xiomara.torresg01@gmail.com>
2025-05-28 08:01:34 +0200

    Update logging.

--- app/logging_config.py
+++ app/logging_config.py
@@ -2,11 +2,12 @@ import logging
 
 def setup_logger(name="Market_Simulation"):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.WARNING)
+    logger.setLevel(logging.INFO)
 
     if not logger.handlers:
         formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s | %(message)s')
         handler = logging.StreamHandler()
+        handler.setLevel(logging.WARNING)
         handler.setFormatter(formatter)
         logger.addHandler(handler)
 
